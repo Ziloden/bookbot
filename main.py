50b111971a6070aff8b2e13d65df5a2608bcb01c
Ziloden <ziloden@pm.me>
2025-08-10 16:45:47 -0700

    Format output

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, sort_char_count
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    book_contents = get_book_text("./books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_contents = get_book_text(book_path)
     num_words = count_words(book_contents)
     num_chars = count_chars(book_contents)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
+    sorted_num_chars = sort_char_count(num_chars)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_num_chars:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict["char"]}: {char_dict["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def count_chars(text):
         else:
             char_count[lower_case_char] = 1
     return char_count
+
+def sort_on(items):
+    return items["num"]
+
+def sort_char_count(char_count):
+    sorted_char_counts = []
+    for char in char_count:
+        sorted_char_counts.append({"char": char, "num": char_count[char]})
+    sorted_char_counts.sort(reverse=True, key=sort_on)
+    return sorted_char_counts
